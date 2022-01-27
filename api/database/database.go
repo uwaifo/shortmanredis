0d5cb483a79602374d96f1f87c887ee9c4023628
uwaifo <mr.uwaifo@gmail.com>
2022-01-27 17:35:59 +0100

    Part Three

--- api/database/database.go
+++ api/database/database.go
@@ -1,1 +1,22 @@
 package database
+
+import (
+	"context"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var Ctx = context.Background()
+
+func CreateClient(dbNo int) *redis.Client {
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("DB_ADDR"),
+		Password: os.Getenv("DB_PASS"), // no password set
+		DB:       dbNo,
+	}).WithContext(Ctx)
+
+	return rdb
+
+}
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,10 +1,33 @@
 package routes
 
 import (
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/uwaifo/shortmanredis/api/database"
 )
 
 func ResolveURL(c *fiber.Ctx) error {
-	return c.SendString("Here in the ResolveURL function")
+
+	url := c.Params("url")
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found in the datanse",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to db",
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter").Err()
+
+	return c.Redirect(value, 301)
 
 }
